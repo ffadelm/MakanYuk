85ccab952effda4732a104ab02f5796e34df1207
ffadelm <faysalfadelm0107@gmail.com>
2022-01-09 21:33:41 +0700

    Halaman Pesan Sukses 🐱‍🏍

---
+++ src/views/PesananSukses.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col text-center">
+          <img src="../assets/images/waiting.svg" alt="" width="300" />
+          <h2 class="mt-5 text-success"><strong>Pesanan Sukses</strong></h2>
+          <h4>
+            Pesanan anda segera diproses <br />
+            Mohon Tunggu Beberapa saat 😊
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  name: "PesananSukses",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
